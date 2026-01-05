a89907f6992916261fde6ee32cb19080a53b360f
Dwain <dwaincontact@gmail.com>
2026-01-05 18:56:43 +0100

    correction

--- app/main.py
+++ app/main.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "app.main:app",
+        app,
         host="0.0.0.0",
         port=port,
         reload=False
